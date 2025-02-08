924d8463c927d3191f213c4ea275a858ee669c6d
Ziaur Chowdhury <ziaur.chowdhury.34@my.csun.edu>
2025-02-08 01:11:06 -0800

    Updated backend settings and added requirements.txt

--- backend/enroll_backend/settings.py
+++ backend/enroll_backend/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Your Django apps
+    'enroll_backend',  # Replace with the actual name of your app
+    'corsheaders',  # Add this for CORS support
 ]
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Add this at the top
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS settings - Allow frontend to connect
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React frontend running locally
+    "http://127.0.0.1:3000",  # Alternative local address
+]
+
